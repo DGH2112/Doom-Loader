e94d709889594376d5b75cef22c64efe1c4b950e
DGH2112 <davidghoyle@gmail.com>
2023-05-20 10:59:17 +0100

    Added GP3 Headers to the main module files

--- Source/DLMainForm.pas
+++ Source/DLMainForm.pas
@@ -3,8 +3,28 @@
   This module contains a form for configuring and launching Doom and its various WADs.
 
   @Author  David Hoyle
-  @Version 12.016
+  @Version 12.129
   @Date    20 May 2023
+
+  @license
+
+    DOOM Loader is a simple application to allow you to select different DOOM
+    game engines, IWADs and PWADs in one single place.
+    
+    Copyright (C) 2023  David Hoyle (https://github.com/DGH2112/Doom-Loader/)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 **)
 Unit DLMainForm;
